b605f3746e25f014144941403be73ab0c698bd04
Angel Davila <adavila0703@gmail.com>
2022-01-09 22:55:04 -0500

    more stuff

--- module-1/labs.py
+++ module-1/labs.py
@@ -8,7 +8,6 @@ Chipotle
 cars
 """
 
-
 # first_name = input()
 # generic_location = input()
 # whole_number = input()
@@ -33,11 +32,56 @@ Enter another integer:
 
 """
 
-user_num = int(input('Enter integer:\n'))
+# user_num = int(input('Enter integer:\n'))
 
-print(f'You entered: {user_num}\n{user_num} squared is {user_num**2}\nAnd {user_num} cubed is {user_num**3} !!' )
+# print(f'You entered: {user_num}\n{user_num} squared is {user_num**2}\nAnd {user_num} cubed is {user_num**3} !!' )
 
-user_num_two = int(input('Enter another integer:\n'))
+# user_num_two = int(input('Enter another integer:\n'))
 
-print(f'{user_num} + {user_num_two} is {user_num_two + user_num}')
-print(f'{user_num} * {user_num_two} is {user_num_two * user_num}')
+# print(f'{user_num} + {user_num_two} is {user_num_two + user_num}')
+# print(f'{user_num} * {user_num_two} is {user_num_two * user_num}')
+
+
+"""
+1.21 LAB: Divide by x
+Write a program using integers user_num and x as input, and output user_num divided by x three times.
+
+Ex: If the input is:
+
+2000
+2
+Then the output is:
+
+1000 500 250
+Note: In Python 3, integer division discards fractions. Ex: 6 // 4 is 1 (the 0.5 is discarded).
+"""
+
+# user_num = int(input())
+# x = int(input())
+# output = ""
+
+# for num in range(0, 3):
+#     user_num /= x
+    
+#     if num == 2:
+#         print("wooop")
+#         output += str(int(user_num))
+#         continue
+        
+#     output += str(int(user_num)) + ' '
+
+# print(output)
+
+'''
+1.22 LAB: Expression for calories burned during workout
+
+'''
+
+user_info = { 'age': 0, 'weight': 0, 'hr': 0, 'time': 0 }
+
+for info in user_info:
+    user_info[info] = input()
+
+calories = (((user_info['age'] * 0.2757) + (user_info['weight'] * 0.03295) + ((user_info['hr'] * 1.0781) - 75.4991)) * user_info['time']) / 8.368
+
+print('Calories: {:.2f} calories'.format(calories))
--- module-1/rando.py
+++ module-1/rando.py
@@ -11,14 +11,29 @@
 
 # print('Sphere volume: {:.2f}'.format(sphere_volume))
 
-G = 6.673e-11
-M = 5.98e24
-accel_gravity = 0.0
+# G = 6.673e-11
+# M = 5.98e24
+# accel_gravity = 0.0
 
-dist_center = 6.3782e6
+# dist_center = 6.3782e6
 
-''' (G * M) / (d 2)  6.3782e6'''
+# ''' (G * M) / (d 2)  6.3782e6'''
 
-accel_gravity = (G * M) / (dist_center**2)
+# accel_gravity = (G * M) / (dist_center**2)
 
-print('Acceleration of gravity: {:.2f}'.format(accel_gravity))
+# print('Acceleration of gravity: {:.2f}'.format(accel_gravity))
+
+
+import math
+
+point_dist = 0.0
+
+x1 = 1.0
+y1 = 2.0
+x2 = 1.0
+y2 = 5.0
+
+
+math.sqrt(math.pow(x2 - x1, 2) + math.pow(y2 - y1, 2))
+
+print('Points distance:', point_dist)
