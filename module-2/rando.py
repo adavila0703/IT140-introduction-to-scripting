63629f8bc61d1ae4689e3fcefd628576a728842e
Angel <adavila0703@gmail.com>
2022-01-16 06:08:22 -0600

    adding unsaved

--- module-2/rando.py
+++ module-2/rando.py
@@ -1,3 +1,13 @@
+from collections import namedtuple
+
+
 rando = [0, 1, 2]
 
-print("hello", rando.sort()[0])
+print("hello", rando.sort())
+
+#Example of nametuple
+team_names = namedtuple('team_names', ['Rockets'])
+
+woop = team_names("test")
+
+woop.Rockets
