240bc7afadec4599ccaa730d494903f237f8c927
Angel <adavila0703@gmail.com>
2022-01-29 16:07:58 -0600

    woo

--- module-4/activities.py
+++ module-4/activities.py
@@ -1,25 +1,16 @@
+
 '''
-Program that has a conversation with the user.
-Uses elif branching and a random number to mix up the program's responses.
+Sample output with inputs: 2 3
+1A 1B 1C 2A 2B 2C 
+
 '''
-import random  # Import a library to generate random numbers
+a, b, c = 4, 5, 20
+mult = 0
+while a < 10:
+    mult = b * a
+    if mult > c:
+        break
+    a = a + 1
+z = a
 
-print('Tell me something about yourself.')
-print('You can type \'Goodbye\' at anytime to quit.\n')
-
-user_text = input()
-
-while user_text != 'Goodbye':
-    random_num = random.randint(0, 2)  # Gives a random number between 0 and 2
-    if random_num == 0:
-        print('\nPlease explain further.\n')
-    elif random_num == 1:
-        print("\nWhy do you say: '{}'?\n".format(user_text))
-    elif random_num == 2:
-        print('\nWhat else can you share?\n')
-    else:
-        print('\nUh-oh, something went wrong. Try again.\n')
-
-    user_text = input()
-
-print('It was nice talking with you. Goodbye.\n')
+print(z)
