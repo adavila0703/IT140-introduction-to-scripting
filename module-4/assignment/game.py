9f8ad9b1b8b6141f8b319a2fdb88c9593d0df409
Angel <adavila0703@gmail.com>
2022-01-30 09:03:33 -0600

    game.py added

--- module-4/assignment/game.py
+++ module-4/assignment/game.py
@@ -40,22 +40,20 @@ def number_guess(guessed_number: int, random_number: int):
     return guessed_number == random_number, 'low' if guessed_number < random_number else 'high'
 
 
-def main():
+def game_start_loop():
     while True:
         print('Welcome to the higher/lower game, Bella!')
         low_num = int(input('Enter the lower bound: '))
-        hight_num = int(input('Enter the upper bound: '))
+        high_num = int(input('Enter the upper bound: '))
 
-        if hight_num > low_num:
+        if high_num > low_num:
             break
 
         print('The lower bound must be less than the upper bound.')
+    return low_num, high_num
 
-    random_number = random.randint(low_num, hight_num)
 
-    guessed_number = int(input(
-        f'\nGreat, now guess a number between {low_num} and {hight_num}: '))
-
+def game_loop(guessed_number: int, random_number: int):
     while True:
         outcome, low_high = number_guess(guessed_number, random_number)
 
@@ -64,8 +62,20 @@ def main():
             break
 
         print(f'Nope, too {low_high}')
+        print('number', random_number)
 
         guessed_number = int(input('\nGuess another number: '))
+
+
+def main():
+    low_num, high_num = game_start_loop()
+
+    random_number = random.randint(low_num, high_num)
+
+    guessed_number = int(input(
+        f'\nGreat, now guess a number between {low_num} and {high_num}: '))
+
+    game_loop(guessed_number, random_number)
 
 
 if __name__ == '__main__':
